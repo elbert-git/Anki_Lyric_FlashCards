2b2bd21db3fe393c4c9d9a98db30a92c083c0534
Elbert Nathanael <elbertwithane@gmail.com>
2023-08-12 16:07:28 +0800

    csv creator done with lyrics

--- dist/main.js
+++ dist/main.js
@@ -14,7 +14,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const createCsv_js_1 = require("./createCsv.js");
 const getLyrics_js_1 = __importDefault(require("./getLyrics.js"));
+const listOfUrls = {
+    alienAlien: 'https://vocaloidlyrics.fandom.com/wiki/%E3%82%A8%E3%82%A4%E3%83%AA%E3%82%A2%E3%83%B3%E3%82%A8%E3%82%A4%E3%83%AA%E3%82%A2%E3%83%B3_(Alien_Alien)',
+    aliceInTheFreezer: 'https://vocaloidlyrics.fandom.com/wiki/%E3%82%A8%E3%82%A4%E3%83%AA%E3%82%A2%E3%83%B3%E3%82%A8%E3%82%A4%E3%83%AA%E3%82%A2%E3%83%B3_(Alien_Alien)',
+    telecasterBboy: 'https://vocaloidlyrics.fandom.com/wiki/%E3%83%86%E3%83%AC%E3%82%AD%E3%83%A3%E3%82%B9%E3%82%BF%E3%83%BC%E3%83%93%E3%83%BC%E3%83%9C%E3%83%BC%E3%82%A4_(Telecaster_B-Boy)',
+};
 (() => __awaiter(void 0, void 0, void 0, function* () {
-    const lyricData = yield (0, getLyrics_js_1.default)('alice in the freezer', 'https://vocaloidlyrics.fandom.com/wiki/Alice_in_%E5%86%B7%E5%87%8D%E5%BA%AB_(Alice_in_Reitouko)');
-    (0, createCsv_js_1.createCsv)(lyricData);
+    Object.keys(listOfUrls).forEach((key) => __awaiter(void 0, void 0, void 0, function* () {
+        const lyricData = yield (0, getLyrics_js_1.default)(key, listOfUrls[key]);
+        (0, createCsv_js_1.createCsv)(lyricData);
+    }));
 }))();
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,16 @@ import getLyrics from "./getLyrics.js";
 // getLyrics('https://vocaloidlyrics.fandom.com/wiki/%E3%82%A8%E3%82%A4%E3%83%AA%E3%82%A2%E3%83%B3%E3%82%A8%E3%82%A4%E3%83%AA%E3%82%A2%E3%83%B3_(Alien_Alien)')
 // getLyrics('https://vocaloidlyrics.fandom.com/wiki/%E3%82%AD%E3%83%A1%E3%83%A9_(Chimera)/DECO*27')
 
+const listOfUrls:{[key:string]:string} = {
+    alienAlien: 'https://vocaloidlyrics.fandom.com/wiki/%E3%82%A8%E3%82%A4%E3%83%AA%E3%82%A2%E3%83%B3%E3%82%A8%E3%82%A4%E3%83%AA%E3%82%A2%E3%83%B3_(Alien_Alien)',
+    aliceInTheFreezer: 'https://vocaloidlyrics.fandom.com/wiki/%E3%82%A8%E3%82%A4%E3%83%AA%E3%82%A2%E3%83%B3%E3%82%A8%E3%82%A4%E3%83%AA%E3%82%A2%E3%83%B3_(Alien_Alien)',
+    telecasterBboy: 'https://vocaloidlyrics.fandom.com/wiki/%E3%83%86%E3%83%AC%E3%82%AD%E3%83%A3%E3%82%B9%E3%82%BF%E3%83%BC%E3%83%93%E3%83%BC%E3%83%9C%E3%83%BC%E3%82%A4_(Telecaster_B-Boy)',
+
+};
+
 (async ()=>{
-    const lyricData = await getLyrics('alice in the freezer', 'https://vocaloidlyrics.fandom.com/wiki/Alice_in_%E5%86%B7%E5%87%8D%E5%BA%AB_(Alice_in_Reitouko)') ;
-    createCsv(lyricData);
+    Object.keys(listOfUrls).forEach(async (key)=>{
+        const lyricData = await getLyrics(key, listOfUrls[key]) ;
+        createCsv(lyricData);
+    })
 })();
